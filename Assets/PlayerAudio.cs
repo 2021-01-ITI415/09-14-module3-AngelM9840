cd232d094d02683eff3182e95093df9fcca2fee4
AngelM9840 <77748592+AngelM9840@users.noreply.github.com>
2021-04-18 11:11:55 -0400

    C# OnTrigger audio

---
+++ Assets/PlayerAudio.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAudio : MonoBehaviour
+{
+    public AudioClip splashSound;
+    public AudioSource audioS;
+
+    private void onTriggerEnter(Collider other) 
+    {
+        if(other.CompareTag("Water"))
+        {
+            audioS.PlayOneShot(splashSound);
+        }
+    }
+
+    private void onTriggerExit(Collider other) 
+    {
+        if(other.CompareTag("Water"))
+        {
+            audioS.PlayOneShot(splashSound);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
