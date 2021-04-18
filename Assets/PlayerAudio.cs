e1f914af92254c451a6bdaadae978b767c3b4308
AngelM9840 <77748592+AngelM9840@users.noreply.github.com>
2021-04-18 11:35:55 -0400

    AudioMixer Snapshot Transitions

--- Assets/PlayerAudio.cs
+++ Assets/PlayerAudio.cs
@@ -1,17 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class PlayerAudio : MonoBehaviour
 {
     public AudioClip splashSound;
     public AudioSource audioS;
+    public AudioMixerSnapshot idleSnapshot;
+    public AudioMixerSnapshot AuxInSnapshot;
 
     private void onTriggerEnter(Collider other) 
     {
         if(other.CompareTag("Water"))
         {
             audioS.PlayOneShot(splashSound);
+        }
+
+        if(other.CompareTag("EnemyZone"))
+        {
+            AuxInSnapshot.TransitionTo(0.5f);
         }
     }
 
@@ -20,6 +28,11 @@ public class PlayerAudio : MonoBehaviour
         if(other.CompareTag("Water"))
         {
             audioS.PlayOneShot(splashSound);
+        }
+
+        if(other.CompareTag("EnemyZone"))
+        {
+            idleSnapshot.TransitionTo(0.5f);
         }
     }
     // Start is called before the first frame update
