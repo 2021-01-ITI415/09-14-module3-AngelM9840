68585366daae1b3d761f0a8bf85552e9cd76fea9
AngelM9840 <77748592+AngelM9840@users.noreply.github.com>
2021-04-22 18:55:51 -0400

    Audio

--- Assets/_Labs/05-Audio/1. Character & NPC Controller/Scripts/MouseAudio.cs
+++ Assets/_Labs/05-Audio/1. Character & NPC Controller/Scripts/MouseAudio.cs
@@ -5,20 +5,10 @@ using UnityEngine;
 public class MouseAudio : MonoBehaviour
 {
     public AudioSource audioS;
+
     public void PlayClip(AudioClip clip)
     {
         audioS.PlayOneShot(clip);
     }
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
